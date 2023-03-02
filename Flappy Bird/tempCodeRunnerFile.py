9758f5ce64ec960a4d1c18bc9820fe6d6270bc9c
utk-ganesh <99419655+utk-ganesh@users.noreply.github.com>
2023-03-02 15:24:10 +0530

    Add files via upload

---
+++ Flappy Bird/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+"""
+The classic game of flappy bird. Make with python
+and pygame. Features pixel perfect collision using masks :o
+
+Date Modified:  Jul 30, 2019
+Author: Tech With Tim
+Estimated Work Time: 5 hours (1 just for that damn collision)
+"""
